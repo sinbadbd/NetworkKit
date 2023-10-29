829627a659f8ec6d76e752a49a5e609cab0139e6
Imran <imran.mia@portonics.com>
2023-10-29 23:30:05 +0600

    Add access level and bug fix

--- Sources/NetworkKit/NetworkKit.swift
+++ Sources/NetworkKit/NetworkKit.swift
@@ -14,7 +14,7 @@ public class NetworkKit {
     
     static public let shared = NetworkKit()
     
-    func request<T: Decodable>(_ endpoint: EndPointConvertible) -> AnyPublisher<T, Error> {
+    public func request<T: Decodable>(_ endpoint: EndPointConvertible) -> AnyPublisher<T, Error> {
         
         var urlRequest = URLRequest(url: URL(string: endpoint.url)!)
         urlRequest.httpMethod = endpoint.method.rawValue
@@ -60,7 +60,7 @@ public class NetworkKit {
         return formatter.string(fromByteCount: bytes)
     }
     
-    func handleCompletion(url: URL, completion: Subscribers.Completion<Error>){
+    public  func handleCompletion(url: URL, completion: Subscribers.Completion<Error>){
         switch completion{
         case .finished:
             
